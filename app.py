e889a66c4d5d6272254dbaddb5328dacba967b49
rjvaghs <113559712+rjvaghs@users.noreply.github.com>
2022-10-14 21:57:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def index():
     appId = 'c29d8e653315ba18a7491286b9d6157b'
 
     #Connect to the API to get IATA Codes
-    api_url = "http://api.aviationstack.com/v1/cities?access_key=" + appId + "&search=" + dep_city
+    api_url = 'http://api.aviationstack.com/v1/cities?access_key=' + appId + '&search=' + dep_city
     headers = {'Content-Type': 'application/json'} #Set the HTTP header for the API request
     response = requests.get(api_url, headers=headers) #Connect to flightlab and read the JSON response.
     iata=response.json() #Convert the JSON string to a dict for easier parsing.
@@ -31,11 +31,4 @@ def index():
     reply = '{"fulfillmentMessages": [ {"text": {"text": ["IATA Code of Airport is '+ dep_iata + '"] } } ]}'
     return reply
 
-test_mode = 0
-port = int(os.environ.get("PORT", 5000))
 
-if __name__ == "main":
-    if test_mode == 1:
-        app.run(debug=False, host='0.0.0.0', port=port)
-    else:
-        app.run(debug=True, use_reloader=True, host='0.0.0.0', port=port)
