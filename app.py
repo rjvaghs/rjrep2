574229b1a83816073715a3e2610011232368de3d
rjvaghs <113559712+rjvaghs@users.noreply.github.com>
2022-10-14 21:29:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ def hello():
 @app.route('/webhook',methods=['POST'])
 def index():
     #Get the geo-city entity from the dialogflow fullfilment request.
-    body = request.json
+    body = request.json()
     dep_city= body['queryResult']['parameters']['dep']
-    
+
 
     appId = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiZTcyNWNlOGRhYjg3NTA0YTlhY2Q0OGM0MDMyYmNkODk3M2RiZTBiYmExMWQ1MDI3OWIxNzNkYjA1NmUyMGM4NWU2YzEwYWIxMjc1ODBmNGYiLCJpYXQiOjE2NjU2MDkyNDIsIm5iZiI6MTY2NTYwOTI0MiwiZXhwIjoxNjk3MTQ1MjQxLCJzdWIiOiIxNDkyNCIsInNjb3BlcyI6W119.m_32z-sDadzteKBWVArGhej-7PS2Cv7bDJ_MEFVA2_lwLFUHdGyLa9xsbu2921Wb28fUaBAjkVyT7dA0BhlkAQ'
 
